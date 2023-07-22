7edb0ba000234aa4012a93f1ec13d3fb347f472b
yayat <yayat.ruhyana@outlook.com>
2023-07-23 00:44:51 +0700

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Yayat</b>
+    </div>
+    <strong>Copyright &copy; 2022-<?php echo date('Y') ?>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
